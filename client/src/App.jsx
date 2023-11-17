5df01d72c518511b6ba4b810bcca0866eeae1ef6
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2023-11-17 17:32:21 +0530

    fixxing bugss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
-/* eslint-disable no-unused-vars */
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+     <h1>hello world</h1>
+    </>
   )
 }
 
